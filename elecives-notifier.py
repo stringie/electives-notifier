14d0674b80509cbfb3a59d8533b688c503ce3b63
stringie <antony3620@gmail.com>
2017-09-30 19:58:35 +0300

    Finilized project.

--- elecives-notifier.py
+++ elecives-notifier.py
@@ -1,34 +1,47 @@
 import requests
 import vlc
 import time
+import threading
+import time
 
-f = open('/home/string/Documents/susihtml.txt', 'r')
-html = f.read()
+def notify():
+    next_call = time.time()
+    while True:
+        f = open('/home/string/Documents/susihtml.txt', 'r')
+        html = f.read()
+        
+        params = {
+            '__VSTATE'          : 'eJz7z8ifws%2fKZWhsamBhYWBgYsmfIsaUhkKIMDHyizHJsYdlFmcm5aRmpDAxA%2fnyDEAGKz%2b%2fGIscv0d%2bUWZVfl5JYo5jTmZ6HreWZnBlcUlqrl54apJeqCeIcgZKF%2bXnFOuhqWWSY4lXDHZiamgQAFoHtAlkFUtIakVJakoKEzvIfHlGbm0WJnkmFDXyzCB5TgLy3ATkeWEe4SegUBCmUJgfykoBAMHgO5k%3d',
+            '__VIEWSTATE'       : '',
+            '__EVENTVALIDATION' : '/wEWBAL+raDpAgKl1bKzCQK1qbSRCwLCi9reAwK9c0s7bLCB8tBILUhZIPSBMlGK',
+            'txtUserName'       : 'akaloferov',
+            'txtPassword'       : '9710134400',
+            'btnSubmit'         : ''
+            }
 
-params = {
-    '__VSTATE'          : 'eJz7z8ifws%2fKZWhsamBhYWBgYsmfIsaUhkKIMDHyizHJsYdlFmcm5aRmpDAxA%2fnyDEAGKz%2b%2fGIscv0d%2bUWZVfl5JYo5jTmZ6HreWZnBlcUlqrl54apJeqCeIcgZKF%2bXnFOuhqWWSY4lXDHZiamgQAFoHtAlkFUtIakVJakoKEzvIfHlGbm0WJnkmFDXyzCB5TgLy3ATkeWEe4SegUBCmUJgfykoBAMHgO5k%3d',
-    '__VIEWSTATE'       : '',
-    '__EVENTVALIDATION' : '/wEWBAL+raDpAgKl1bKzCQK1qbSRCwLCi9reAwK9c0s7bLCB8tBILUhZIPSBMlGK',
-    'txtUserName'       : 'akaloferov',
-    'txtPassword'       : '9710134400',
-    'btnSubmit'         : ''
-    }
+        with requests.Session() as s:
+            s.post('https://susi.uni-sofia.bg/ISSU/forms/Login.aspx', data=params)
+            open_page = s.get('https://susi.uni-sofia.bg/ISSU/forms/students/ElectiveDisciplinesSubscribe.aspx')
 
-with requests.Session() as s:
-    s.post('https://susi.uni-sofia.bg/ISSU/forms/Login.aspx', data=params)
-    open_page = s.get('https://susi.uni-sofia.bg/ISSU/forms/students/ElectiveDisciplinesSubscribe.aspx')
+            susi = open_page.text.encode('utf-8').strip()
+            if html == susi:
+                print "No change"
+            else:
+                print "Changed"
+                instance = vlc.Instance('--input-repeat=9999')
+                player = instance.media_player_new()
+                media = instance.media_new("/home/string/Music/cuckoo-clock.mp3")
+                player.set_media(media)
+                player.play()
+                time.sleep(10)
+                player.stop()
+                f.close()
+                l = open('/home/string/Documents/susihtml.txt', 'w')
+                l.write(susi)
+                next_call = time.time()
 
-    susi = open_page.text.encode('utf-8').strip()
-    if html == susi:
-        print "No change"
-    else:
-        print "Changed"
-        instance = vlc.Instance('--input-repeat=9999')
-        player = instance.media_player_new()
-        media = instance.media_new("/home/string/Music/cuckoo-clock.mp3")
-        player.set_media(media)
-        player.play()
-        time.sleep(100)
-        f.close()
-        l = open('/home/string/Documents/susihtml.txt', 'w')
-        l.write(susi)
+        next_call = next_call+60;
+        time.sleep(next_call - time.time())
+
+timerThread = threading.Thread(target=notify)
+timerThread.start()
